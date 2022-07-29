b56e0035ccda99a67ae0a238f1c137664d3e7d01
Dan Ko <danko56666@gmail.com>
2022-07-28 23:57:04 -0400

    fix lint warnings

--- lib/auth/view/sign_in_page.dart
+++ lib/auth/view/sign_in_page.dart
@@ -1,14 +1,12 @@
 import 'package:chingu_bookfinder_flutter/auth/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: CustomBackground(),
       ),
