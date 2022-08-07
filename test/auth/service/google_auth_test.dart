6a64deaf6f0526fcbfac3538f4a200555497eeda
Dan Ko <danko56666@gmail.com>
2022-08-06 23:59:08 -0400

    add unit tests for google auth service

--- test/auth/service/google_auth_test.dart
+++ test/auth/service/google_auth_test.dart
@@ -1,17 +1,20 @@
-// import 'package:chingu_bookfinder_flutter/auth/auth.dart';
+import 'package:chingu_bookfinder_flutter/auth/auth.dart';
 import 'package:firebase_api/firebase_api.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
 
 class MockGoogleAuth extends Mock implements GoogleAuth {}
 
 void main() {
-  // late GoogleAuthService googleAuthService;
-  // late GoogleAuth googleAuth;
+  group('google auth service', () {});
+  late GoogleAuthService googleAuthService;
+  late GoogleAuth googleAuth;
 
-  setUp(() async {
-    // googleAuth = MockGoogleAuth();
-    // googleAuthService = GoogleAuthService();
+  setUp(() {
+    googleAuth = MockGoogleAuth();
+    googleAuthService = GoogleAuthService(googleAuth: googleAuth);
+
+    when(() => googleAuth.signOut()).thenAnswer((_) async => _);
   });
 
   // group('constructor', () {
@@ -20,5 +23,32 @@ void main() {
   //   });
   // });
 
-  // group('sign in', () {});
+  group('sign in', () {
+    test('is called succesfully', () async {
+      when(() => googleAuth.signIn()).thenAnswer((_) async => '1234');
+
+      await googleAuthService.signIn();
+      verify(() => googleAuth.signIn()).called(1);
+    });
+
+    test('returns instance of sign in with google failure on error', () async {
+      when(() => googleAuth.signIn()).thenThrow(
+        SignInWithGoogleFailure.fromCode(''),
+      );
+
+      expect(
+        () async => googleAuthService.signIn(),
+        throwsA(isA<SignInWithGoogleFailure>()),
+      );
+    });
+  });
+
+  group('sign out', () {
+    test('is called succesfully', () async {
+      when(() => googleAuth.signOut()).thenAnswer((_) async => _);
+
+      await googleAuthService.signOut();
+      verify(() => googleAuth.signOut()).called(1);
+    });
+  });
 }
